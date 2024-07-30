e913e21ef918acf3e0fcf2dba0dd8942ccfa1920
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-07-30 11:09:56 -0300

    Criado classe posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace XadrezConsole
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao p;
 
+            p = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {p}");
+
+            Console.ReadLine();
 
         }
     }
---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrezConsole.Tabuleiro
+{
+    internal class Posicao
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Posicao() 
+        {
+        }
+
+        public Posicao(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line + ", " + Column;
+        }
+    }
+}
